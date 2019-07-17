45c208e5f602a60ea84776b1132337625d06b60d
Sammy <asfernandes@sbcglobal.net>
2019-07-17 17:52:14 -0400

    added data endpoints and stylying of website

--- javascript/app.js
+++ javascript/app.js
@@ -2,58 +2,145 @@
 
 
 const handleData = (data) => {
-    
-let simpleData = data._embedded['city:search-results'][0];
 
-let cityName = simpleData.matching_full_name ;
+    console.log(`Data results: ${data}`);
+    
+    
+let myData = data._embedded['city:search-results'][0];
+let cityName = myData.matching_full_name; //find the city Name in the returned object
 
 const $cityTitle = $('<h2>');
-$cityTitle.text(cityName);
-console.log($cityTitle);
+$cityTitle.text(cityName) //prints the city name on the webpage
 
 $('#city-section').removeClass('hidden')
+$('#title').append($cityTitle);
 
-$('.jumbotron').after($cityTitle);
+const geoLink = myData['_links']['city:item']['href'] //retrieves geographical information needed for additional api
 
-let geoLink = simpleData['_links']['city:item']['href']
+    //////////////////////////////////////////////////////////////////////
+    ////// FETCHING GEODATA INFORMATION TO DISPLAY OTHER DETAILS /////////
+    //////////////////////////////////////////////////////////////////////
 
-console.log(data);//shows city data
-console.log(cityName); //logs city name
-console.log(geoLink);//logs geotag link
+    const useGeoData = (geodata) => {
+        let urban_areaLink = geodata._links['city:urban_area']['href'] //find the nearest urban area to display info
+        let urbanAreaName = geodata._links['city:urban_area']['name']
 
-const useGeoData = (data) => {
-    let geoNameId = 5391959;
-    let urban_areaLink = `data._links['city:urban_area']`
-    console.log(urban_areaLink);
-    }
-$.ajax({url: urban_areaLink}).then(setTimeout(3000, useGeoData))
+        console.log(geodata);
+
+        // console.log(urban_areaLink);
+        // console.log(`${urban_areaLink}images`);
+        
+        /////////////////////////////////////////////////////////////////
+        ///// remove default embedded map and replace with new iframe////
+        /////////////////////////////////////////////////////////////////
+
+        const lat = geodata.location.latlon.latitude
+        const lon = geodata.location.latlon.longitude
+        console.log(`lat is ${lat} lon is ${lon}`);
+
+        $('#map').empty()
+        $('#map').html(
+       `     <iframe class='map' 
+            width="400" 
+            height="400" 
+            frameborder="0" 
+            scrolling="no" 
+            marginheight="0" 
+            marginwidth="0" 
+            src='https://maps.google.com/maps?q=${lat},${lon}&hl=es;z=18&amp;output=embed'>
+            </iframe>`)
+
+        /// --> saving this information for a strech goal to display map later ///
+
+        ////// DISPLAYING IMAGES //////////////////////
+        const displayImages = (imageData) => {
+        const cityImage = imageData.photos[0]['image']['web'];
+        console.log(imageData);
+        
+        $('.jumbotron').css('background-image', `url(${cityImage})`)
+        $('.jumbotron').css('height', `20em`)
+ 
+        }
+
+
+        ////// DISPLAYING SCORES INFORMATION ////////////////////
+        const displayScores = (scoreData) => {
+            // console.log(scoreData);
+
+            const $citySummary = $(scoreData.summary);
+            const $urban = $('<p>')
+            $urban.text(`${cityName} is part of the ${urbanAreaName} urban area.`)
+            
+            $cityTitle.after($citySummary);
+            $cityTitle.after($urban);
+
+            const categoriesArr = scoreData.categories
+            // console.log(categoriesArr);
+
+            
+            for (let i = 0; i < categoriesArr.length; i++){
+                let curCategory = categoriesArr[i];
+                let catName = curCategory.name;
+                let catScore = Math.round(curCategory.score_out_of_10);
+                let catColor = curCategory.color;
+
+                const $div = $('<div>')
+                $div.addClass('scores')
+                $div.attr('id', `scoreDiv` + i)
+                $('#city-scores').append($div)
+
+                
+                const $p = $('<p>')
+                $p.text(`${catName}: `); //text for each category
+                $(`#scoreDiv${i}`).append($p);
+
+                
+                const $bar = $('<div>') //gray bar to style
+                $bar.css('width', '100px')
+                $bar.css('height', '14px')
+                $bar.css('border', '1px solid black')
+                $bar.css('position', 'relative')
+                $(`#scoreDiv${i}`).append($bar)
+
+
+                const $colorWithinBar = $('<div>') //colored bar represeting rating within
+                $colorWithinBar.css('width', `${catScore}0px`)
+                $colorWithinBar.css('height', '14px')
+                $colorWithinBar.css('background-color', `${catColor}`)
+                $colorWithinBar.css('position', 'absolute')
+                $bar.append($colorWithinBar)
+
+                const $p2 = $('<p>')
+                $p2.text(`${catScore} of 10 `); //text and value for category score          
+                $(`#scoreDiv${i}`).append($p2)
+            }
+
+            let $sectionName = $('<h3>')
+            $sectionName.text('City Ratings')
+            $("#city-scores").prepend($sectionName)
+         
+        }
     
+        $.ajax({url: `${urban_areaLink}scores`}).then(displayScores)
+        $.ajax({url: `${urban_areaLink}images`}).then(displayImages)
+
+    }
+$.ajax({url: geoLink}).then(useGeoData) 
+
 }
 
 
 
 $('.submit-btn').on('click', (event) =>{
     event.preventDefault();
+
+    $('#title').empty();
+    $('#city-scores').empty();
     const city = $('.text-box').val().toLowerCase();
     console.log(city);
     
     const endpoint = `https://api.teleport.org/api/cities/?search=${city}&limit=1`
     $.ajax({url: endpoint}).then(handleData)
+
 })
-
-///// Get Scores /////
-
-// https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/scores/
-// example of data.categories (extracted from categories array)
-// {
-//     "color": "#f3d630",
-//     "name": "Cost of Living",
-//     "score_out_of_10": 2.618
-//   }
-// `https://api.teleport.org/apicities/?search=${cityname}&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurbanarea%2Fua%3Ascores`
-
-//// Get Photos ///////
-// `https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/images`
-
-//// API Reference //////
 
